3c77f3dabc796bb42c210491b9b38380eaa9f4d7
Tomás Ampuero <46233260+tampueroc@users.noreply.github.com>
2023-07-04 14:11:53 -0400

    Initial commit

---
+++ src/main/java/com/cc5002/spring_app/SpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.cc5002.spring_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppApplication.class, args);
+	}
+
+}
